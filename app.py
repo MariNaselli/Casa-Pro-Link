45f4d5488e44eaba4124995f80da52173f458e09
Mariana Naselli <nasellimariana@outlook.com>
2026-01-14 16:35:52 -0300

    Estructura db

--- app.py
+++ app.py
@@ -6,11 +6,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///casaprolink.db'
 db = SQLAlchemy(app)
 
-# Definimos cómo es una "Propiedad" en nuestra base de datos
 class Propiedad(db.Model):
+    # Control Interno
     id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100))
+    
+    # Comercial (Público)
+    titulo = db.Column(db.String(100), nullable=False)
+    operacion = db.Column(db.String(20))  # Venta o Alquiler
     precio = db.Column(db.Integer)
+    expensas = db.Column(db.Integer)
+    ubicacion = db.Column(db.String(200))
+    descripcion = db.Column(db.Text)
+    
+    # Ficha Técnica
+    m2_totales = db.Column(db.Integer)
+    m2_cubiertos = db.Column(db.Integer)
+    dormitorios = db.Column(db.Integer)
+    banios = db.Column(db.Integer)
+    
+    # Datos Privados
+    propietario_nombre = db.Column(db.String(100))
+    propietario_tel = db.Column(db.String(50))
+    notas_internas = db.Column(db.Text)
 
 # Creamos la base de datos físicamente
 with app.app_context():
@@ -29,16 +46,23 @@ def procesar():
     # Capturamos lo que el usuario escribió en los inputs
     titulo_recibido = request.form.get('titulo')
     precio_recibido = request.form.get('precio')
+    ubicacion_recibida = request.form.get('ubicacion')
+    propietario_recibido = request.form.get('propietario')
     
     # 2. Creamos un nuevo objeto "Propiedad" con esos datos
-    nueva_propiedad = Propiedad(titulo=titulo_recibido, precio=precio_recibido)
+    nueva_propiedad = Propiedad(
+        titulo=titulo_recibido, 
+        precio=precio_recibido,
+        ubicacion=ubicacion_recibida,
+        propietario_nombre=propietario_recibido
+        )
     
     # 3. Lo guardamos en la base de datos
     db.session.add(nueva_propiedad)
     db.session.commit()
     
     # Por ahora, solo lo mostramos en pantalla para ver que funciona
-    return f"<h1>¡Guardado con éxito!</h1><p>La propiedad '{titulo_recibido}' ya está en la base de datos.</p><a href='/cargar'>Cargar otra</a>"
+    return f"<h1>¡Guardado!</h1><p>Propiedad: {titulo_recibido} en {ubicacion_recibida} cargada correctamente.</p><a href='/cargar'>Cargar otra</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
